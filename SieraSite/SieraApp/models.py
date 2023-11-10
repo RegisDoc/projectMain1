a238eae08c75b5c2273332e355f74ae8eb6a260c
RegisDoc <regitahiri121@gmail.com>
2023-11-09 18:12:22 -0800

    second commit

--- SieraSite/SieraApp/models.py
+++ SieraSite/SieraApp/models.py
@@ -1,15 +1,17 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
+class CarManager(models.Manager):
+    def create_car(self, model, price_per_hour, image):
+        car = self.model(model=model, price_per_hour=price_per_hour, image=image)
+        car.save()
+        return car
 
-
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField("date published")
-
-
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+ class Car(models.Model):
+    model = models.CharField(max_length=100)
+    price_per_hour = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(upload_to='image')
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_rented = models.BooleanField(default=False)
+    
--- SieraSite/SieraApp/views.py
+++ SieraSite/SieraApp/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def home(request):
     return render(request,"home.html")
+def about(request):
+    return render(request,"about.html")
+def cars(request):
+    return render(request,"cars.html")
+def contact(request):
+    return render(request,"contact.html")
+def rental(request):
+    return render(request,"rental.html")
+
