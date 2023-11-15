ed96c17a803351126cdd5bdcfb33f360f5514342
RegisDoc <regitahiri121@gmail.com>
2023-11-15 14:32:01 -0800

    first commit

--- SieraSite/SieraApp/models.py
+++ SieraSite/SieraApp/models.py
@@ -8,10 +8,21 @@ class CarManager(models.Manager):
         car.save()
         return car
 
- class Car(models.Model):
+class Car(models.Model):
     model = models.CharField(max_length=100)
     price_per_hour = models.DecimalField(max_digits=6, decimal_places=2)
-    image = models.ImageField(upload_to='image')
+    image = models.ImageField(upload_to='car_images')
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
     is_rented = models.BooleanField(default=False)
-    
+   
+    objects = CarManager()
+
+    def __str__(self):
+        return self.model
+class CartItem(models.Model):
+    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity} x {self.car.model}"
--- SieraSite/SieraApp/views.py
+++ SieraSite/SieraApp/views.py
@@ -12,3 +12,17 @@ def contact(request):
 def rental(request):
     return render(request,"rental.html")
 
+
+def homepage(request):
+    search_query = request.GET.get("q", "")
+    cars = Car.objects.all()
+
+    if search_query:
+        cars = cars.filter(model__icontains=search_query)
+
+    context = {
+        "cars": cars,
+        "search_query": search_query,
+    }
+
+    return render(request, "app/homepage.html", context)
